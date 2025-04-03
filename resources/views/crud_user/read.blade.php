6815bf65ea4c3780c8887cfcec1af25ecdb78082
TrashCan <phuthuytinhyeu1234678@gmail.com>
2025-04-03 11:25:48 +0700

    update read page

--- resources/views/crud_user/read.blade.php
+++ resources/views/crud_user/read.blade.php
@@ -2,24 +2,33 @@
 
 @section('content')
     <main class="login-form">
-        <div class="container">
+        <div class="container mt-5">
             <div class="row justify-content-center">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{$messi->id}}</td>
-                            <td>{{$messi->name}}</td>
-                            <td>{{$messi->email}}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="col-md-8">
+                    <div class="card shadow-lg rounded">
+                        <div class="card-header bg-primary text-white text-center">
+                            <h3>User Details</h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped table-bordered text-center">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{{$messi->id}}</td>
+                                        <td>{{$messi->name}}</td>
+                                        <td>{{$messi->email}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
